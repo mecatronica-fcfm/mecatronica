450898e32a6dd1b4e593f5bf4923999855297aad
Mecatronica <mecatronicafcfm@gmail.com>
2016-06-23 17:57:26 -0400

    archivos que hizo rodrigo

--- ros/biped_dummy_plug/src/biped_dummy_plug/biped_dummy_plug_node.py
+++ ros/biped_dummy_plug/src/biped_dummy_plug/biped_dummy_plug_node.py
@@ -54,6 +54,17 @@ def callback_right_ankle(value):
 	rospy.loginfo("Dummy: Right Ankle: %d !", int(value))
 	return
 
+def callback_enable(value):
+	value = int(value)
+	if value==1:
+		value = True
+	else:
+		value = False
+	msg_servo_array.enable = value
+	pub_servo_array.publish(msg_servo_array)
+	rospy.loginfo("Enable: " + str(value))
+	return
+
 #Initialize as node
 rospy.init_node('biped_dummy_plug_node', anonymous=True)
 
@@ -65,12 +76,19 @@ msg_servo_array.left_ankle_angle  = 128
 msg_servo_array.right_hip_angle   = 128
 msg_servo_array.right_knee_angle  = 128
 msg_servo_array.right_ankle_angle = 128
+msg_servo_array.enable = True
+
+def quit(event):
+    rospy.loginfo("Exit!")
+    rospy.signal_shutdown("")
+    root.quit()
 
 #Create & Configure Publishers
 pub_servo_array = rospy.Publisher('biped_dummy_plug_node/t_servo_array', ServoArray, queue_size=10)
 
 #Create main window
 root = Tk()
+root.bind('<Control-c>', quit)
 root.minsize() #width=500, height=500
 root.wm_title("Biped - EVA-01 Dummy Plug")
 
@@ -97,33 +115,37 @@ Label(win, text='Ankle:').grid(in_=frame_left,row=2,column=0, sticky=W)
 Label(win, text='Hip:').grid(in_=frame_right,row=0,column=0, sticky=W)
 Label(win, text='Knee:').grid(in_=frame_right,row=1,column=0, sticky=W)
 Label(win, text='Ankle:').grid(in_=frame_right,row=2,column=0, sticky=W)
-
+Label(win, text='Enable:').grid(in_=frame_right,row=3,column=0, sticky=W)
 
 #Add Slide Bars
 
-left_hip_bar = Scale(win, from_=0, to=255, orient=HORIZONTAL, command=callback_left_hip)
+left_hip_bar = Scale(win, from_=10, to=170, orient=HORIZONTAL, command=callback_left_hip)
 left_hip_bar.grid(in_=frame_left, row=0, column=100, sticky=E)
-left_hip_bar.set(128)
+left_hip_bar.set(106)
 
-left_knee_bar = Scale(win, from_=0, to=255, orient=HORIZONTAL, command=callback_left_knee)
+left_knee_bar = Scale(win, from_=10, to=170, orient=HORIZONTAL, command=callback_left_knee)
 left_knee_bar.grid(in_=frame_left, row=1, column=100, sticky=E)
-left_knee_bar.set(128)
+left_knee_bar.set(112)
 
-left_ankle_bar = Scale(win, from_=0, to=255, orient=HORIZONTAL, command=callback_left_ankle)
+left_ankle_bar = Scale(win, from_=10, to=170, orient=HORIZONTAL, command=callback_left_ankle)
 left_ankle_bar.grid(in_=frame_left, row=2, column=100, sticky=E)
-left_ankle_bar.set(128)
+left_ankle_bar.set(93)
 
-right_hip_bar = Scale(win, from_=0, to=255, orient=HORIZONTAL, command=callback_right_hip)
+right_hip_bar = Scale(win, from_=10, to=170, orient=HORIZONTAL, command=callback_right_hip)
 right_hip_bar.grid(in_=frame_right, row=0, column=100, sticky=E)
-right_hip_bar.set(128)
+right_hip_bar.set(97)
 
-right_knee_bar = Scale(win, from_=0, to=255, orient=HORIZONTAL, command=callback_right_knee)
+right_knee_bar = Scale(win, from_=10, to=170, orient=HORIZONTAL, command=callback_right_knee)
 right_knee_bar.grid(in_=frame_right, row=1, column=100, sticky=E)
-right_knee_bar.set(128)
+right_knee_bar.set(91)
 
-right_ankle_bar = Scale(win, from_=0, to=255, orient=HORIZONTAL, command=callback_right_ankle)
+right_ankle_bar = Scale(win, from_=10, to=170, orient=HORIZONTAL, command=callback_right_ankle)
 right_ankle_bar.grid(in_=frame_right, row=2, column=100, sticky=E)
-right_ankle_bar.set(128)
+right_ankle_bar.set(97)
+
+enable_bar = Scale(win, from_=0, to=1, orient=HORIZONTAL, command=callback_enable)
+enable_bar.grid(in_=frame_right, row=3, column=100, sticky=E)
+enable_bar.set(0)
 
 win.mainloop()
 
--- ros/biped_dummy_plug_arduino/biped_dummy_plug/biped_dummy_plug.ino
+++ ros/biped_dummy_plug_arduino/biped_dummy_plug/biped_dummy_plug.ino
@@ -21,14 +21,21 @@ Servo rightAnkleServo;
 void messageCb( const c_msg::ServoArray& msg)
 {
   leftHipServo.write(msg.left_hip_angle);
+  leftKneeServo.write(msg.left_knee_angle);
+  leftAnkleServo.write(msg.left_ankle_angle);
+  rightHipServo.write(msg.right_hip_angle);
+  
+  rightKneeServo.write(msg.right_knee_angle);
+  rightAnkleServo.write(msg.right_ankle_angle);
+  digitalWrite(7,msg.enable);
 }
    
 ros::Subscriber<c_msg::ServoArray> sub("/biped_dummy_plug_node/t_servo_array", &messageCb );
    
 void setup()
 {
-  pinMode(13, OUTPUT);
-  
+  pinMode(7, OUTPUT);
+  digitalWrite(7,LOW);
   // Init ROS Node and Subscriber
   nh.initNode();
   nh.subscribe(sub);
